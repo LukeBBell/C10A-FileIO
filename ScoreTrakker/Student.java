4ad123214d18d2701b9d5e718dfb20bd780281c0
Cade Shiveley <cade_shiveley@mines.edu>
2025-10-02 13:13:50 -0600

    adding comments

--- ScoreTrakker/Student.java
+++ ScoreTrakker/Student.java
@@ -1,12 +1,13 @@
 public class Student {
+    //initializing the name and score
     private String name;
     private int score;
-
+// constructor for the name and score
     public Student(String name, int score){
         this.name = name;
         this.score = score;
     }
-
+//to string to print out  the name and score
     @Override
     public String toString(){
         //System.out.println(name + " " + score);
